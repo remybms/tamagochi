5ad0580fd7625e8ae061decbdf1ad6c228162af4
remybms <bamasremy43@gmail.com>
2023-05-16 08:27:43 +0200

    start basic class for tamagochi

---
+++ tamagochi/src/main/java/tamagochi/tamago.java
@@ -0,0 +1,14 @@
+package tamagochi;
+
+public class tamago {
+    private String name = "";
+    private Number age = 0;
+    private Number hungerLevel = 0;
+    private Number funLevel = 0;
+    private boolean sickness = false;
+    private boolean dirty = false;
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
