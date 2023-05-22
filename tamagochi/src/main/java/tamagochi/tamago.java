a1e03fe4217985c65207170b7c913860f45106ec
remybms <bamasremy43@gmail.com>
2023-05-22 09:15:03 +0200

    avoid bug

--- tamagochi/src/main/java/tamagochi/tamago.java
+++ tamagochi/src/main/java/tamagochi/tamago.java
@@ -2,11 +2,11 @@ package tamagochi;
 
 public class tamago {
     private String name = "";
-    private Number age = 0;
-    private Number hungerLevel = 0;
-    private Number funLevel = 15;
-    private boolean sickness = false;
-    private boolean dirty = false;
+    private int age = 0;
+    public int hungerLevel = 0;
+    public int funLevel = 15;
+    public boolean sickness = false;
+    public boolean dirty = false;
 
     public void setName(String name){
         this.name = name;
