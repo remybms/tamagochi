bc1fa556aeb89d63dbdc722c7d34ba7bbe0984ea
remybms <bamasremy43@gmail.com>
2023-05-22 11:06:59 +0200

    add feeding, block funLevel<50, add messages

--- tamagochi/src/main/java/tamagochi/Life.java
+++ tamagochi/src/main/java/tamagochi/Life.java
@@ -4,14 +4,30 @@ public class Life {
     tamago tamagochi = new tamago();
 
     public void Play(){
-        tamagochi.funLevel += 3;
+        if(tamagochi.funLevel <= 47){
+            tamagochi.funLevel += 3;
+        } else {
+            tamagochi.funLevel = 50;
+        }
+        System.out.println("Vous vous êtes bien amusés");
     }
 
     public void Heal(){
         tamagochi.sickness = false;
+        System.out.println("Il a été soigné");
     }
 
     public void Clean(){
         tamagochi.dirty = false;
+        System.out.println("Il a été néttoyé");
+    }
+
+    public void Feed(){
+        if(tamagochi.hunger == true){
+            tamagochi.hunger = false;
+            System.out.println("Il a été nourri");
+        } else {
+            System.out.println("Il n'a plus faim");
+        }
     }
 }
