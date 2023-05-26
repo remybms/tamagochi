bb2ebeb1e9b41b4742170d3c6692f3c8fc1472fa
remybms <bamasremy43@gmail.com>
2023-05-26 11:34:59 +0200

    delete age

--- tamagochi/src/main/java/tamagochi/tamago.java
+++ tamagochi/src/main/java/tamagochi/tamago.java
@@ -2,7 +2,6 @@ package tamagochi;
 
 public class tamago {
     private String name = "";
-    private int age = getCycleCount();
     public boolean hunger = false;
     public int funLevel = 15;
     public boolean sickness = false;
