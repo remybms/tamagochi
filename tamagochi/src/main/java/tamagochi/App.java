eb9481e2765bceae7518b855845c3b6213629bf4
remybms <bamasremy43@gmail.com>
2023-05-26 09:47:06 +0200

    add main function

--- tamagochi/src/main/java/tamagochi/App.java
+++ tamagochi/src/main/java/tamagochi/App.java
@@ -8,6 +8,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int choice = -1;
+        Life life = new Life();
+        do{
+            choice = life.Menu();
+            switch(choice){
+                case 1 :
+                life.Feed();
+                break;
+                case 2 :
+                life.Play();
+                break;
+                case 3 :
+                life.Clean();
+                break;
+                case 4 :
+                life.Heal();
+                break;
+            }
+        }while(choice != 0);
     }
 }
