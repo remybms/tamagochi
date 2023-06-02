e110580894f8c0984c38d112fa66ac02dfa8bdb4
remybms <bamasremy43@gmail.com>
2023-06-02 08:17:29 +0200

    add dying loop

--- tamagochi/src/main/java/tamagochi/commandLine.java
+++ tamagochi/src/main/java/tamagochi/commandLine.java
@@ -4,37 +4,38 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class commandLine {
-    public static void main( String[] args ){
+    public static void main(String[] args) {
         int choice = -1;
-        Life life = new Life();
-        life.setName(Life.prompt("Comment voulez-vous l'appeler ?"));
-        life.Menu();
-        Timer timer = new Timer();
+        do {
+            Life life = new Life();
+            life.setName(Life.prompt("Comment voulez-vous appeler votre nouveau tamagochi ?"));
+            Timer timer = new Timer();
 
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                System.out.println("Une nouvelle journée commence");
-                life.lifeCycling();
-            }
-        };
-        timer.schedule(task, 0, 60000);
-        do{
-            choice = life.Menu();
-            switch(choice){
-                case 1 :
-                life.Feed();
-                break;
-                case 2 :
-                life.Play();
-                break;
-                case 3 :
-                life.Clean();
-                break;
-                case 4 :
-                life.Heal();
-                break;
-            }
-        }while(choice != 0);
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("Une nouvelle journée commence");
+
+                }
+            };
+            timer.schedule(task, 0, 60000);
+            do {
+                choice = life.Menu();
+                switch (choice) {
+                    case 1:
+                        life.Feed();
+                        break;
+                    case 2:
+                        life.Play();
+                        break;
+                    case 3:
+                        life.Clean();
+                        break;
+                    case 4:
+                        life.Heal();
+                        break;
+                }
+            } while (choice != 0 && life.tamagochi.state != "dead");
+        } while (choice != 0);
     }
 }
