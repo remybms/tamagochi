2d09e22e8ae1c487557684c2c4f5acadb771b11a
remybms <bamasremy43@gmail.com>
2023-06-07 10:40:02 +0200

    start setting up javafx game

--- graphique/src/main/java/tamagochigraphique/App.java
+++ graphique/src/main/java/tamagochigraphique/App.java
@@ -4,34 +4,72 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    private static Scene scene;
+    public static Life life = new Life();
+    public static Timer timer;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
+    public void start(Stage stage) throws IOException{
+
+        Button feedButton = new Button("feed");
+        Button playButton = new Button("play");
+        Button cleanButton = new Button("clean");
+        Button healButton = new Button("heal");
+        Button quit = new Button("quit");
+        VBox box = new VBox();
+        box.getChildren().addAll(feedButton, playButton, cleanButton, healButton, quit);
+        feedButton.setOnMouseClicked(e -> {
+            life.Feed();
+        });
+        playButton.setOnMouseClicked(e -> {
+            life.Play();
+        });
+        cleanButton.setOnMouseClicked(e -> {
+            life.Clean();
+        });
+        healButton.setOnMouseClicked(e -> {
+            life.Heal();
+        });
+        quit.setOnMouseClicked(e -> {
+            timer.cancel();
+        });
+        Scene mainScene = new Scene(box, 400,400);
+        stage.setScene(mainScene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
+        timer = new Timer();
+
+        class MyTimerTask extends TimerTask {
+            private Life currentLife;
+    
+            public MyTimerTask(Life life) {
+                currentLife = life;
+            }
+    
+            @Override
+            public void run() {
+                System.out.println("Une nouvelle journée commence");
+                currentLife.tamagochi.age++;
+                currentLife.lifeCycling();
+            }
+        }
+    
+        TimerTask task = new MyTimerTask(life);
+        timer.schedule(task, 0, 10000);
         launch();
     }
 
